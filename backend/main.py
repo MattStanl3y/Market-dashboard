f7e01756b2f8072cc4af1c557a2457f8ad3fffaf
Matt Stanley <Mattstanley2389@gmail.com>
2025-07-30 19:07:23 -0700

    minor change

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://market-dashboard-frontend.vercel.app"
+        "https://market-dashboard-fawn.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
